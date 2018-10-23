b18300613d8a45048da78889ed968d714030a54a
zoomi-raja <zamurd@awok.ae>
2018-10-23 15:21:36 +0400

    logo added for login

--- app/src/main/java/com/example/android/savemydata/Auth/AuthActivity.java
+++ app/src/main/java/com/example/android/savemydata/Auth/AuthActivity.java
@@ -10,5 +10,6 @@ public class AuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        getSupportActionBar().hide();
     }
 }
