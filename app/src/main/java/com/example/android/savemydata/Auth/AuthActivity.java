3a7b8f4e2b7513212b564996cbea915ead4c8f27
zoomi-raja <zamurd@awok.ae>
2018-10-25 17:57:57 +0400

    login screen basic

--- app/src/main/java/com/example/android/savemydata/Auth/AuthActivity.java
+++ app/src/main/java/com/example/android/savemydata/Auth/AuthActivity.java
@@ -1,15 +1,18 @@
 package com.example.android.savemydata.Auth;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.android.savemydata.R;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private  ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        getSupportActionBar().hide();
+        viewPager = findViewById(R.id.view_pager);
     }
 }
---
+++ app/src/main/java/com/example/android/savemydata/Auth/SetPasswordFragment.java
@@ -0,0 +1,18 @@
+package com.example.android.savemydata.Auth;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.android.savemydata.R;
+
+public class SetPasswordFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.set_password_fragment,container,false);
+    }
+}
